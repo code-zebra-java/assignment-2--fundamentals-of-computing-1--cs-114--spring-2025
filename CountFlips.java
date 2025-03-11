212b0e5cdafa17471891017283295d14d51df547
code-zebra-java <73904245+code-zebra-java@users.noreply.github.com>
2025-03-11 11:07:25 -0400

    went through my code and commented and fixed typos

--- CountFlips.java
+++ CountFlips.java
@@ -1,10 +1,13 @@
 public class CountFlips {
   public static void main(String[] args) {
+    // create a new instance of the coin class
     Coin myCoin = new Coin();
 
+    //create variables to store the counts of each face
     int headsCount = 0;
     int tailsCount = 0;
 
+    //run a for loop that counts how many times each face is flipped
     for (int i=0; i<100; i++){
       myCoin.flip();
 
@@ -15,7 +18,7 @@ public class CountFlips {
       }
     }
 
+    // print the results to the user
     System.out.println("Count of Heads flipped: " + headsCount + " Count of Tails flipped: " + tailsCount);
-    System.out.println();
   }
 }
--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -5,8 +5,8 @@ public class PrintVowelsAndNonVowels {
     Scanner scan = new Scanner(System.in);
 
     //initialize the counters
-    int aCounter = 0, eCounter = 0, iCounter = 0, oCounter = 0,
-        uCounter = 0, nonVowelCounter = 0, spaceCounter = 0;
+    int a_Counter = 0, e_Counter = 0, i_Counter = 0, o_Counter = 0,
+        u_Counter = 0, nonVowelCounter = 0, spaceCounter = 0;
 
     //print a prompt to the user and accept the user input
     System.out.print("Please type in a sentence:");
@@ -15,19 +15,19 @@ public class PrintVowelsAndNonVowels {
     //run a  for loop that sorts the different types of letter to their respective counters
     for (int i=0; i < userInput.length(); i++){
       if (userInput.charAt(i) == 'a'){
-        aCounter++;
+        a_Counter++;
       }
       else if (userInput.charAt(i) == 'e'){
-        eCounter++;
+        e_Counter++;
       }
       else if (userInput.charAt(i) == 'i'){
-        iCounter++;
+        i_Counter++;
       }
       else if (userInput.charAt(i) == 'o'){
-        oCounter++;
+        o_Counter++;
       }
       else if (userInput.charAt(i) == 'u'){
-        uCounter++;
+        u_Counter++;
       }
       //this excludes the spaces from the nonvowel counter
       else if (userInput.charAt(i) == ' '){
@@ -39,11 +39,11 @@ public class PrintVowelsAndNonVowels {
     }
 
     //Print out the counts of each vowel and nonvowels to the user
-    System.out.println("The vowel \"a\" appears in your sentence " + aCounter + " times");
-    System.out.println("The vowel \"e\" appears in your sentence " + eCounter + " times");
-    System.out.println("The vowel \"i\" appears in your sentence " + iCounter + " times");
-    System.out.println("The vowel \"o\" appears in your sentence " + oCounter + " times");
-    System.out.println("The vowel \"u\" appears in your sentence " + uCounter + " times");
+    System.out.println("The vowel \"a\" appears in your sentence " + a_Counter + " times");
+    System.out.println("The vowel \"e\" appears in your sentence " + e_Counter + " times");
+    System.out.println("The vowel \"i\" appears in your sentence " + i_Counter + " times");
+    System.out.println("The vowel \"o\" appears in your sentence " + o_Counter + " times");
+    System.out.println("The vowel \"u\" appears in your sentence " + u_Counter + " times");
     System.out.println("There are " + nonVowelCounter + " nonvowels in your sentence");
 
     scan.close();
