b3907bf60f52f427ad54367e415c953876f54e5a
code-zebra-java <73904245+code-zebra-java@users.noreply.github.com>
2025-03-11 00:34:11 -0400

    completed the program for the print vowels and non-vowels class

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,4 +1,7 @@
+
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
+
+
   }
 }
--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -1,4 +1,51 @@
+import java.util.Scanner;
+
 public class PrintVowelsAndNonVowels {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    //initialize the counters
+    int aCounter = 0, eCounter = 0, iCounter = 0, oCounter = 0,
+        uCounter = 0, nonVowelCounter = 0, spaceCounter = 0;
+
+    //print a prompt to the user and accept the user input
+    System.out.print("Please type in a sentence:");
+    String userInput = scan.nextLine();
+
+    //run a  for loop that sorts the different types of letter to their respective counters
+    for (int i=0; i < userInput.length(); i++){
+      if (userInput.charAt(i) == 'a'){
+        aCounter++;
+      }
+      else if (userInput.charAt(i) == 'e'){
+        eCounter++;
+      }
+      else if (userInput.charAt(i) == 'i'){
+        iCounter++;
+      }
+      else if (userInput.charAt(i) == 'o'){
+        oCounter++;
+      }
+      else if (userInput.charAt(i) == 'u'){
+        uCounter++;
+      }
+      //this excludes the spaces from the nonvowel counter
+      else if (userInput.charAt(i) == ' '){
+        spaceCounter++;
+      }
+      else{
+        nonVowelCounter++;
+      }
+    }
+
+    //Print out the counts of each vowel and nonvowels to the user
+    System.out.println("The vowel \"a\" appears in your sentence " + aCounter + " times");
+    System.out.println("The vowel \"e\" appears in your sentence " + eCounter + " times");
+    System.out.println("The vowel \"i\" appears in your sentence " + iCounter + " times");
+    System.out.println("The vowel \"o\" appears in your sentence " + oCounter + " times");
+    System.out.println("The vowel \"u\" appears in your sentence " + uCounter + " times");
+    System.out.println("There are " + nonVowelCounter + " nonvowels in your sentence");
+
+    scan.close();
   }
 }
