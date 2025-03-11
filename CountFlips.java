94b8fdb005a0e04a3718cb2b4e462035186dadf8
code-zebra-java <73904245+code-zebra-java@users.noreply.github.com>
2025-03-10 21:28:57 -0400

    completed the code for count flips

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,21 @@
 public class CountFlips {
   public static void main(String[] args) {
+    Coin myCoin = new Coin();
+
+    int headsCount = 0;
+    int tailsCount = 0;
+
+    for (int i=0; i<100; i++){
+      myCoin.flip();
+
+      if (myCoin.isHeads()){
+        headsCount++;
+      }else{
+        tailsCount++;
+      }
+    }
+
+    System.out.println("Count of Heads flipped: " + headsCount + " Count of Tails flipped: " + tailsCount);
+    System.out.println();
   }
 }
