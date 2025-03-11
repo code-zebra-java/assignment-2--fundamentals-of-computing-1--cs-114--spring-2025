45cc9c9a20f7500753585c14e8838af06419a43b
code-zebra-java <73904245+code-zebra-java@users.noreply.github.com>
2025-03-11 01:15:54 -0400

    Completed the Hundred bottles of beer

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -2,6 +2,11 @@
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
 
-
+    //run a for loop that prints each verse of the “One Hundred Bottles of Beer” song.
+    for (int i=100; i > 0; i--){
+      System.out.println(i + " bottles of beer on the wall \n" + i + 
+      " bottles of beer \nIf one of those bottles should happen to fall \n"+ 
+       (i-1) + " bottles of beer on the wall \n");
+    }
   }
 }
